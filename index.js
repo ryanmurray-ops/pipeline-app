a74595b10c9db2545cbcd338d61fb133d2a160bc
Ryan Murray <ryan.murray@madetech.com>
2026-03-27 10:29:20 +0000

    Added API routes

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express'); // Load Express library
 const app = express(); // Create an app instance
+
 const PORT = 3000; // Define the port number
 
+// Root route
 app.get('/', (req, res) => { // define a route for GET requests at '/'
-    res.send('Hello World!') // Send this message as a response
+    res.json({ message: 'Welcome to my API'}) // Send this message as a response
 });
+
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'Ryan' },
+        { id: 2, name: 'Alex' }
+    ];
+    res.json(users)
+});
+
+app.get('/status', (req, res) => {
+    res.json({ status: 'Server is running' })
+})
 
 app.listen(PORT, () => { // Start the server
     console.log(`Server is running on http://localhost:${PORT}`)
